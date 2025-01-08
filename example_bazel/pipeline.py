96c57df330d4ea3cd64c59e24e5048c344b53dc7
Josh <josh@sematic.dev>
2025-01-08 15:27:24 +0000

    add ray test

--- example_bazel/pipeline.py
+++ example_bazel/pipeline.py
@@ -1,6 +1,23 @@
+import ray
 import sematic
+import time
+from sematic.ee.ray import RayCluster, RayNodeConfig, SimpleRayCluster
 
 
-@sematic.func
+@ray.remote
+def do_it(x: int) -> int:
+    print(f"Doing {x}")
+    return 2*x
+
+
+@sematic.func(standalone=True)
 def pipeline():
-    pass
+    with RayCluster(
+        config=SimpleRayCluster(
+            n_nodes=2,
+            node_config=RayNodeConfig(cpu=1, memory_gb=2),
+        )
+    ):
+        print(ray.get([do_it.remote(16)]))
+    print("Done!!!!")
+
